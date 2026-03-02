f220d39e51fbd9efcd50949de00faf95157478ff
Jahanvi Chahan <jahanvichauhan69@gmail.com>
2026-03-02 15:24:26 +0530

    hero section done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { navLinks } from "../constants"
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav className="">
+            <img src="/logo.svg" alt="apple_logo" />
+            <ul>
+            {navLinks.map(({label})=>(
+               <li key={label}>
+                <a href={label}>{label}</a>
+               </li>
+            ))}
+            </ul>
+            <div>
+                  <button>
+             <img src="/search.svg" alt="search" />
+           </button>
+
+            <button>
+             <img src="/cart.svg" alt="cart" />
+           </button>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const navLinks=
+[
+                { label:'Store'},
+                { label:'Mac'},
+                { label:'iPhone'},
+                { label:'Vision'},
+                { label:'Watch'},
+                { label:'airPods'},
+              
+            ]
