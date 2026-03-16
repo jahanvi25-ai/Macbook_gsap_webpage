b836b997a9a8bcce74b77a39d977559f344eeb08
Jahanvi Chahan <jahanvichauhan69@gmail.com>
2026-03-16 09:36:31 +0530

    footer done, very last part of the page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,27 @@
-
+import { footerLinks } from "../constants"
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="info">
+       <p>More ways to shop: Find an apple store or other retailer. Or call 000800 040 1966</p>
+       <img src="/logo.svg" alt="apple logo" />
+      </div>
+        <hr />
+      <div className="links">
+        <p>Copyright © 2024 Apple Inc. All right reserved.</p>
+
+        <ul>
+          {
+           footerLinks.map(({label,link})=>(
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+           ))
+          }
+        </ul>
+      </div>
+
+    </footer>
   )
 }
 
